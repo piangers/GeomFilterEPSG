2470ccd0524c41e6d61134f5e831e8a51724f61f
Cesar Piangers <cesarpiangers@gmail.com>
2018-07-26 14:12:24 -0300

    V 0.1

--- GeomFilterEPSG.py
+++ GeomFilterEPSG.py
@@ -10,17 +10,17 @@ class GeomFilterEPSG():
     
 
 	def __init__(self, iface):
-        
-        self.iface = iface
-        
-    def initGui(self): 
-
-        self.toolbar = self.iface.addToolBar("My_ToolBar")
+		
+		self.iface = iface
+		
+	def initGui(self): 
+		
+		self.toolbar = self.iface.addToolBar("My_ToolBar")
         # cria uma ação que iniciará a configuração do plugin 
         pai = self.iface.mainWindow()
-        icon_path = ':/plugins/MeasureArea/icon.png'
-        self.action = QAction (QIcon (icon_path),u"Calcula em metros quadrados, tamanho da area ou areas.", pai)
-        self.action.setObjectName ("Retorna a area do poligono.")
+        icon_path = ':/plugins/GeomFilterEPSG/icon.png'
+        self.action = QAction (QIcon (icon_path),u"filtro.", pai)
+        self.action.setObjectName ("filtro.")
         self.action.setStatusTip(None)
         self.action.setWhatsThis(None)
         self.action.setCheckable(True)
@@ -30,90 +30,89 @@ class GeomFilterEPSG():
         self.textbox.setFixedWidth(120)
         # Add textbox to toolbar
         self.toolbar.addAction(self.action)
+		
         self.toolbar.addWidget(self.textbox)
         # Set tooltip
         self.action.toggled.connect(self.enableElements)
         self.enableElements(False)
         self.textbox.textChanged.connect(self.enableTool) # acho que é aqui o sinal a ser trabalhado
         self.action.toggled.connect(self.enableTool)                                           
-  
+		
+		
+		
 		# Create new virtual layer 
-		vlyr = QgsVectorLayer("Polygon", "temporary_polygons", "memory")
-		dprov = vlyr.dataProvider()
+
+	self.vlyr = QgsVectorLayer("Polygon?crs=EPSG:31982", "temporary_polygons", "memory")
+	self.dprov = self.vlyr.dataProvider()
 
 		# Add field to virtual layer 
-		dprov.addAttributes([QgsField("name", QVariant.String),
-						QgsField("size", QVariant.Double)])
+	self.dprov.addAttributes([QgsField("name", QVariant.String),
+							QgsField("size", QVariant.Double)])
 
-		vlyr.updateFields()
+	self.vlyr.updateFields()
 
+	self.myRubberBand = QgsRubberBand( iface.mapCanvas(), QGis.Polygon )
+	self.color = QColor(78, 97, 114)
+	self.color.setAlpha(190)
+	self.myRubberBand.setColor(color)
+				
 		# Access MapTool  
-		previousMapTool = iface.mapCanvas().mapTool()
-		myMapTool = QgsMapToolEmitPoint( iface.mapCanvas() )
+	self.previousMapTool = iface.mapCanvas().mapTool()
+	self.myMapTool = QgsMapToolEmitPoint( iface.mapCanvas() )
 
+		# VALORES ANTES PASSADOS EM FORMA DE SCRIPT
+	self.myMapTool.canvasClicked.connect( mouseClick )
+	self.iface.mapCanvas().setMapTool( myMapTool )
 
 		# create empty list to store coordinates 
-		coordinates = []
+	coordinates = []
 
 		# Access ID 
-		fields = dprov.fields() 
+	self.fields = self.dprov.fields() 
 
 	def drawBand( currentPos, clickedButton ):
-	    iface.mapCanvas().xyCoordinates.connect( drawBand )
+		self.iface.mapCanvas().xyCoordinates.connect( drawBand )
 
-	    if myRubberBand and myRubberBand.numberOfVertices():
-		myRubberBand.removeLastPoint()
-		myRubberBand.addPoint( currentPos )
+		if self.myRubberBand and self.myRubberBand.numberOfVertices():
+			self.myRubberBand.removeLastPoint()
+			self.myRubberBand.addPoint( QgsPoint(currentPos) )
 
 
 	def mouseClick( currentPos, clickedButton ):
-	    if clickedButton == Qt.LeftButton and len(coordinates) == 0: 
-		# create the polygon rubber band associated to the current canvas
-		global myRubberBand 
-		myRubberBand = QgsRubberBand( iface.mapCanvas(), QGis.Polygon )
-		# set rubber band style
-		color = QColor(78, 97, 114)
-		color.setAlpha(190)
-		myRubberBand.setColor(color)
-		#Draw rubberband
-		myRubberBand.addPoint( QgsPoint(currentPos) )
-		coordinates.append(currentPos)
-		print coordinates
-	    if clickedButton == Qt.LeftButton and len(coordinates) > 0:
-		myRubberBand.addPoint( QgsPoint(currentPos) )
-		coordinates.append(currentPos)
-		print coordinates
+		if clickedButton == Qt.LeftButton:# and myRubberBand.numberOfVertices() == 0: 
+			myRubberBand.addPoint( QgsPoint(currentPos) )
+			
+		elif clickedButton == Qt.RightButton and myRubberBand.numberOfVertices() > 3:
 
-	    if clickedButton == Qt.RightButton:
+			# open input dialog     
+			(description, False) = QInputDialog.getText(iface. mainWindow(), "Description", "Description for Polygon at x and y", QLineEdit.Normal, 'My Polygon') 
 
-		 # open input dialog     
-		(description, False) = QInputDialog.getText(iface.mainWindow(), "Description", "Description for Polygon at x and y", QLineEdit.Normal, 'My Polygon') 
+			#create feature and set geometry             
+			poly = QgsFeature() 
+			geomP = myRubberBand.asGeometry()
+			poly.setGeometry(geomP) 
+			
+			print myRubberBand.numberOfVertices()
 
-		#create feature and set geometry             
-		poly = QgsFeature() 
-		geomP = QgsGeometry.fromPolygon([coordinates])
-		poly.setGeometry(geomP) 
+			#set attributes
+			indexN = dprov.fieldNameIndex('name') 
+			indexA = dprov.fieldNameIndex('size') 
+			poly.setAttributes([QgsDistanceArea().measurePolygon(coordinates), indexA])
+			poly.setAttributes([description, indexN])
 
-		#set attributes
-		indexN = dprov.fieldNameIndex('name') 
-		indexA = dprov.fieldNameIndex('size') 
-		poly.setAttributes([QgsDistanceArea().measurePolygon(coordinates), indexA])
-		poly.setAttributes([description, indexN])
+			# add feature                 
+			self.dprov.addFeatures([poly])
+			self.vlyr.updateExtents()
 
-		# add feature                 
-		dprov.addFeatures([poly])
-		vlyr.updateExtents()
+			#add layer      
+			self.vlyr.triggerRepaint()
+			QgsMapLayerRegistry.instance().addMapLayers([vlyr])
+			self.myRubberBand.reset(QGis.Polygon)
 
-		#add layer  
-		    
-		# iface.mapCanvas().refresh()
-		vlyr.triggerRepaint()
-		QgsMapLayerRegistry.instance().addMapLayers([vlyr])
+#myMapTool.canvasClicked.connect( mouseClick )
+#iface.mapCanvas().setMapTool( myMapTool )
 
-		#delete list
-		del coordinates[:]
 
     
 
-	myMapTool.canvasClicked.connect( mouseClick )
-	iface.mapCanvas().setMapTool( myMapTool )
+	
